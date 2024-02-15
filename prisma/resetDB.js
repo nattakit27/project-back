e9a1085dd35f005a4107a8380e9ad1e25766f6ab
nattakit27 <643170010218@rmu.ac.th>
2024-02-16 03:08:57 +0700

    project-back

--- prisma/resetDB.js
+++ prisma/resetDB.js
@@ -2,8 +2,8 @@ const {PrismaClient} =require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function run() {
-  await prisma.$executeRawUnsafe('DROP Database ccac01_connect')
-  await prisma.$executeRawUnsafe('CREATE Database ccac01_connect')
+  await prisma.$executeRawUnsafe('DROP Database project-dental')
+  await prisma.$executeRawUnsafe('CREATE Database project-dental')
 }
 console.log('Reset DB')
 run()
